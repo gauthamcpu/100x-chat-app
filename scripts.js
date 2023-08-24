cd98a500478bd047ddb0fcd725c63db6f1028d44
gauthamcpu <138781317+gauthamcpu@users.noreply.github.com>
2023-08-24 19:53:08 +0530

    some errors happened at this point
    i stopped because some errors happened

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// const socket = io(); // Connect to the server
+
+// const form = document.getElementById('form');
+// const input = document.getElementById('input');
+// const messages = document.getElementById('messages');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   if (input.value) {
+//     socket.emit('chat message', input.value); // Send the message to the server
+//     input.value = '';
+//   }
+// });
+
+// socket.on('chat message', (msg) => {
+//   const li = document.createElement('li');
+//   li.textContent = msg;
+//   messages.appendChild(li);
+// });
